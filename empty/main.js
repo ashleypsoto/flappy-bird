b4a0b8780fc7279e786b1103185b4e5c597b8584
ashleypsoto <soto.ashley97@gmail.com>
2016-07-15 13:12:09 -0400

    chdibbf

--- empty/main.js
+++ empty/main.js
@@ -40,10 +40,12 @@ var mainState = {
       // Call the 'restartGame' function
       if (this.bird.y < 0 || this.bird.y > 490)
             this.restartGame();
+            game.physics.arcade.overlap(
+            this.bird, this.pipes, this.restartGame, null, this);
+
     },
 
-    game.physics.arcade.overlap(
-    this.bird, this.pipes, this.restartGame, null, this);
+
       // Make the bird jump
   jump: function() {
       // Add a vertical velocity to the bird
